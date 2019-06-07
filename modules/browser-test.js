c205395a1085091b1eacac57996636dc4b546c45
Daniel J. Lewis <mail@TheAudacitytoPodcast.com>
2019-06-07 00:31:06 -0400

    Set timeout to 2 hours

--- modules/browser-test.js
+++ modules/browser-test.js
@@ -23,7 +23,7 @@ app.get('/', (req,res) => {
 
 app.post('/chart', (req, res) => {
     time.start('timer');
-    req.setTimeout((30*60*1000));
+    req.setTimeout((120*60*1000));
     const query = req.body;
     let testOptions = {
         title: query.title,
